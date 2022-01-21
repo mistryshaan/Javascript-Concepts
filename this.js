d67b8067c57ec62172d42c5f87e516b2f927f872
mistryshaan <shaan.mistry@marutitech.com>
2022-01-21 10:52:41 +0530

    Fundamental topic

--- this.js
+++ this.js
@@ -1,7 +1,38 @@
 console.log(this); // {}
 
+//////////////////////////////////////////////////////////////////
+
 function sayName() {
   console.log(this);
 }
 
 sayName(); // <ref *1> Object [global]
+
+//////////////////////////////////////////////////////////////////
+
+const user = {
+  firstName: "Shaan",
+  lastName: "Mistry",
+  age: 21,
+  location: "Ahmedabad",
+  getUserInfo: function() {
+    console.log(this);
+    
+    sayHello();
+
+    function sayHello() {
+      console.log(this); // <ref *1> Object [global]
+    }
+  }
+}
+
+user.getUserInfo();
+/*
+{
+  firstName: 'Shaan',
+  lastName: 'Mistry',
+  age: 21,
+  location: 'Ahmedabad',
+  getUserInfo: [Function: getUserInfo]
+}
+*/
